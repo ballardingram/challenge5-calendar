e60ad29c1664d0f901434f9b4b44dd9619e10ebc
Ballard Ingram <ballardingram@outlook.com>
2022-05-20 14:08:52 -0500

    body - updated task structure and icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 // FUNCTION > SETS CURRENT DATE
 // DOCUMENTATION > NOW DAYJS (https://day.js.org/docs/en/parse/now)
-//const now = new Date ();
-//const Date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-//document.getElementById("todaysDate").innerHTML = date;
+// DOCUMENATION > CURRENT TIME (https://phoenixnap.com/kb/how-to-get-the-current-date-and-time-javascript)
+// NOTE > THIS CODE WAS PUSHED TO GITHUB AS A PART OF THE HTML FOR TESTING, THEN MOVED TO SCRIPT.JS
+const today = new Date();
+            const date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
+            const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+            const dateTime = date + ' ' + time;
+            document.getElementById("CurrentDate").innerHTML = dateTime;
 
 // FUNCTION > TIME OF DAY STYLE INDICATOR - CHANGES TASK APPEARANCE TO REFLECT OVERDUE (PAST), CURRENT (PRESENT), AND UPCOMING(FUTURE)
 const hourIndicator = function() {
-    const currentTime = moment().hour()
+    const today = new Date();
+    const currentTime = today.getHours();
     for ( const i = 7; i < 19; i++) {
-        const taskCard = $("#task-" +i)
+        const taskCard = $("#time-" +i)
         if(currentTime > i) {
             $(taskCard).addClass("past");
         } else if (currentTime === i) {
